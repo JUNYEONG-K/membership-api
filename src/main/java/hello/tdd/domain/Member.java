7eb3f450fdf8afc33f0667b2acb2077c9c37d478
JUNYEONG-K <godoly1211@naver.com>
2022-09-05 16:44:27 +0900

    db 연동 테스트

---
+++ src/main/java/hello/tdd/domain/Member.java
@@ -0,0 +1,21 @@
+package hello.tdd.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String username;
+    private String password;
+
+}
